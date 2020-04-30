f74ba0618230d53274001c7d74e6815e2ad8fcd2
Nicolas Eduardo Pardo Arias <nicolasparias26@gmail.com>
2020-04-30 08:51:27 -0500

    Adding BIT or Fenwick

---
+++ DATA STRUCTURES/BinaryIndexedTree.cpp
@@ -0,0 +1,24 @@
+//Complexity: O(log(n))
+// Indexed 1 to n
+int tree[N]; //N size of the array
+int query(int k){
+	int sum = 0;
+	while(k>=1){
+		sum+=tree[k];
+		k -= k&-k;
+	}
+	return sum;
+}
+void update(int k, int x){
+	while(k<=n){
+		tree[k]+=x;
+		k += k&-k;
+	}
+}
+int query_range(int l, int r){
+	return query(r)-query(l-1);
+}
+void update_range(int l, int r, int x){
+	update(r+1,-x);
+	update(l,x);
+}
