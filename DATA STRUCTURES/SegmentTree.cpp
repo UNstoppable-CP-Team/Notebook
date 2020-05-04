2dafe091ee18a373d4a02781f29a0342569ed908
Nicolas Arias <30639530+NicolasArias@users.noreply.github.com>
2020-05-04 12:16:04 -0500

    Update SegmentTree.cpp

--- DATA STRUCTURES/SegmentTree.cpp
+++ DATA STRUCTURES/SegmentTree.cpp
@@ -3,14 +3,14 @@ int seg[2*n + 100], arr[n];
 void add(int x, int k){ //add can be any operation min, max ...
 	k+=n;
 	seg[k] = x;
-	for(k/=2; k>=1; k/=2) seg[k] = seg[k] + seg[2*k];
+	for(k/=2; k>=1; k/=2) seg[k] = seg[2*k] + seg[2*k+1];
 }
 int sum(int a, int b){ //Just calculates the result in the given range
 	a+=n, b+=n;
 	int ans = 0;
 	while(a<=b){
-		if(a%2) ans += seg[a++];
-		if(b%2 == 0) ans +=seg[b--];
+		if(a&1) ans += seg[a++];
+		if(!(b&1)) ans +=seg[b--];
 		a/=2, b/=2;
 	}
 	return ans;
