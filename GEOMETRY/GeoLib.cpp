fe48a4edceb4593ca56c6db03c48aaa487b1a305
Nicolas Arias <30639530+NicolasArias@users.noreply.github.com>
2020-08-07 17:55:51 -0500

    Update and rename Point.cpp to GeoLib.cpp
    New methods

--- GEOMETRY/GeoLib.cpp
+++ GEOMETRY/GeoLib.cpp
@@ -11,3 +11,16 @@ struct pt {
 	T cross (pt p) { return x * p.y - y * p.x;}
 
 };
+T dot(pt a, pt b){
+	return a.x*b.x+a.y*b.y;
+}
+T cross(pt a, pt b){
+	return a.x*b.y-a.y*b.x;
+}
+double area(vector<pt> p){
+	T ans = 0;
+	int n = p.size();
+	for(int i=0; i<n; i++)
+		ans += cross(p[i],p[(i+1)%n]);
+	return abs(ans)/2.0;
+}
