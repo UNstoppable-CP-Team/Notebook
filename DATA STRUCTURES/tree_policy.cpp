470e3754efeebaf4406a3ab31572646f0c18b451
Nicolas Arias <30639530+NicolasArias@users.noreply.github.com>
2020-08-14 20:06:35 -0500

    Update tree_policy.cpp

--- DATA STRUCTURES/tree_policy.cpp
+++ DATA STRUCTURES/tree_policy.cpp
@@ -1,5 +1,6 @@
 #include <ext/pb_ds/assoc_container.hpp> 
 #include <ext/pb_ds/tree_policy.hpp> 
 using namespace __gnu_pbds; 
-typedef int T;
-#define ordered_set tree<T,null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>
+typedef tuple<int,int,int> T;
+typedef tree<T,null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+
