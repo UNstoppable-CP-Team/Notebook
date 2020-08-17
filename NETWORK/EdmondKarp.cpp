c879486020896afd9d3ab8d090d85354e2e37f3a
Jherson Medina <38172045+JhersonMedina@users.noreply.github.com>
2020-08-17 16:00:58 -0500

    struct Edkarp

--- NETWORK/EdmondKarp.cpp
+++ NETWORK/EdmondKarp.cpp
@@ -5,36 +5,48 @@
 	Call ans = maxflow(s, t) int the main
 */
 //Complexity O(n*m*m)
-int bfs(int s, int t, vector<int>& parent) {
-	parent.assign(maxn, -1);
-	parent[s] = -2;
-	queue<pii> q;
-	q.push({s, oo});
-	while(!q.empty()) {
-		int u = q.front().first, flow = q.front().second;
-		q.pop();
-		for(int v : graph[u]) {
-			if(parent[v] == -1 && capacity[u][v]) {
-				parent[v] = u;
-				int newflow = min(flow, capacity[u][v]);
-				if(v == t) return newflow;
-				q.push({v, newflow});
+struct  Edkarp {
+	int n, s, t;
+	vector< vector<int> > graph, capacity;
+	Edkarp(int n, int s, int t) : n(n), s(s), t(t) {
+		graph.resize(n);
+		capacity.assign(n, vector<int> (n, 0));
+	}
+	void addedge(int u, int v, int c) {
+		graph[u].push_back(v), graph[v].push_back(u);
+		capacity[u][v] = c, capacity[v][u] = 0;
+	}
+	int bfs(vector<int>& parent) {
+		parent.assign(n, -1);
+		parent[s] = -2;
+		queue<pii> q;
+		q.push({s, oo});
+		while(!q.empty()) {
+			int u = q.front().first, flow = q.front().second;
+			q.pop();
+			for(int v : graph[u]) {
+				if(parent[v] == -1 && capacity[u][v]) {
+					parent[v] = u;
+					int newflow = min(flow, capacity[u][v]);
+					if(v == t) return newflow;
+					q.push({v, newflow});
+				}
 			}
 		}
+		return 0;
 	}
-	return 0;
-}
-int maxflow(int s, int t) {
-	int flow = 0, newflow;
-	vector <int> parent;
-	while(newflow = bfs(s, t, parent)) {
-		flow += newflow;
-		int cur = t;
-		while(cur != s) {
-			capacity[parent[cur]][cur] -= newflow;
-			capacity[cur][parent[cur]] += newflow;
-			cur = parent[cur];
+	int maxflow() {
+		int flow = 0, newflow;
+		vector <int> parent;
+		while(newflow = bfs(parent)) {
+			flow += newflow;
+			int cur = t;
+			while(cur != s) {
+				capacity[parent[cur]][cur] -= newflow;
+				capacity[cur][parent[cur]] += newflow;
+				cur = parent[cur];
+			}
 		}
+		return flow;
 	}
-	return flow;
 }
