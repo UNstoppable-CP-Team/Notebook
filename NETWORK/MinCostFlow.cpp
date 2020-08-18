3bbb79e5479570f89a7baa5a9580a9810048099a
Jherson Medina <38172045+JhersonMedina@users.noreply.github.com>
2020-08-17 21:50:45 -0500

    creating struct (not tested yet xd)

--- NETWORK/MinCostFlow.cpp
+++ NETWORK/MinCostFlow.cpp
@@ -8,56 +8,63 @@
 */
 //Complexity O(n*n*m*m)
 struct Edge{
-	int from, to, capacity, cost;
+	int from, to, cap, cost;
 };
-void spfa(int s, int n, vector<int>& distance, vector<int>& parent) {
-	distance.assign(n, oo);
-	parent.assign(n, -1);
-	vector<bool> inq(n, 0);
-	distance[s] = 0, inq[s] = 1;
-	queue<int> q;
-	q.push(s);
-	while(!q.empty()) {
-		int u = q.front(); q.pop();
-		inq[u] = 0;
-		for(int v : graph[u]) {
-			if(capacity[u][v] > 0 && distance[v] > distance[u] + cost[u][v]) {
+struct MCF{
+	int n, s, t;
+	vector< vector <int> > graph, capacity, cost;
+	MCF (int n, int s, int t) : n(n), s(s), t(t), graph(n) {
+		capacity.assign(n, vector <int> (n));
+		cost.assign(n, vector <int> (n));
+	}
+	void addedge(int u, int v, int cap, int c) {
+		graph[u].push_back(v), graph[v].push_back(u);
+		capacity[u][v] = cap, capacity[v][u] = 0;
+		cost[u][v] = c, cost[v][u] = -c;
+	}
+	void spfa(int s, int n, vector<int>& distance, vector<int>& parent) {
+		distance.assign(n, oo);
+		parent.assign(n, -1);
+		vector<bool> inq(n, 0);
+		distance[s] = 0, inq[s] = 1;
+		queue<int> q;
+		q.push(s);
+		while(!q.empty()) {
+			int u = q.front(); q.pop();
+			inq[u] = 0;
+			for(int v : graph[u]) {
+				if(capacity[u][v] > 0 && distance[v] > distance[u] + cost[u][v]) {
 				distance[v] = distance[u] + cost[u][v];
-				parent[v] = u;
-				if(!inq[v]) {
-					inq[v] = 1;
-					q.push(v);
+					parent[v] = u;
+					if(!inq[v]) {
+						inq[v] = 1;
+						q.push(v);
+					}
 				}
 			}
 		}
 	}
-}
-int mincostflow(int n, vector<Edge>& edges, int K, int s, int t) {
-	for(auto e : edges) {
-		int u = e.from, v = e.to, w = e.capacity, c = e.cost;
-		graph[u].push_back(v), graph[v].push_back(u);
-		capacity[u][v] = w, capacity[v][u] = 0;
-		cost[u][v] = c, cost[v][u] = -c;
-	}
-	int flow = 0, cost = 0;
-	vector <int> distance, parent;
-	while(flow < K) {
-		spfa(s, n, distance, parent);
-		if(distance[t] == oo) break;
-		int f = K - flow, cur = t;
-		while (cur != s) {
-			f = min(f, capacity[parent[cur]][cur]);
-			cur = parent[cur];
+	int mincostflow(int K) {
+		int flow = 0, cost = 0;
+		vector <int> distance, parent;
+		while(flow < K) {
+			spfa(s, n, distance, parent);
+			if(distance[t] == oo) break;
+			int f = K - flow, cur = t;
+			while (cur != s) {
+				f = min(f, capacity[parent[cur]][cur]);
+				cur = parent[cur];
+			}
+			flow += f;
+			cost += f * distance[t];
+			cur = t;
+			while(cur != s) {
+				capacity[parent[cur]][cur] -= f;
+				capacity[cur][parent[cur]] += f;
+				cur = parent[cur];
+			}
 		}
-		flow += f;
-		cost += f * distance[t];
-		cur = t;
-		while(cur != s) {
-			capacity[parent[cur]][cur] -= f;
-			capacity[cur][parent[cur]] += f;
-			cur = parent[cur];
-		}
+		if(flow < K) return -1;
+		return cost;
 	}
-	if(flow < K) return -1;
-	return cost;
-}
+};
