dcb413a793cececd054e2fde247991945633ff3e
Nicolas Arias <30639530+NicolasArias@users.noreply.github.com>
2021-01-06 08:19:13 -0500

    Update SegmentTreeBottomUP.cpp

--- DATA STRUCTURES/SegmentTreeBottomUP.cpp
+++ DATA STRUCTURES/SegmentTreeBottomUP.cpp
@@ -1,20 +1,22 @@
-int n; //size of the array
-int seg[2*n + 100], arr[n];
-void add(int x, int k){ //add can be any operation min, max ...
-	k+=n;
-	seg[k] = x;
-	for(k>>=1; k>=1; k>>=1) seg[k] = seg[(k<<1)] + seg[(k<<1|1)];
-}
-int sum(int a, int b){ //Just calculates the result in the given range
-	a+=n, b+=n;
-	int ans = 0;
-	while(a<=b){
-		if(a&1) ans += seg[a++];
-		if(!(b&1)) ans +=seg[b--];
-		a>>=1, b>>=1;
+struct Stree{
+	int n;
+	vector<int> tree;
+	Stree (int n) : n(n) {
+		tree.assign(2*n+100,1e9+10);
 	}
-	return ans;
-}
-void build(){
-  for(int i=0; i<n; ++i) add(arr[i], i);
-}
+	void update(int k, int x){
+		k += n;
+		tree[k] = x;
+		for(k>>=1; k; k>>=1) tree[k] = min(tree[k<<1],tree[(k<<1)|1]);
+	}
+	int query(int l, int r){
+		int ans = 1e9+10;
+		l += n, r+=n;
+		while(l<=r){
+			if(l&1) ans = min(ans,tree[l++]);
+			if(!(r&1)) ans = min(ans,tree[r--]);
+			l>>=1,r>>=1;
+		}
+		return ans;
+	}
+};
