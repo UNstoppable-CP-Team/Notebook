1329ba7696a5e1fa7840f5dbf890601c657d7f66
Jherson Medina <38172045+JhersonMedina@users.noreply.github.com>
2021-07-19 18:42:31 -0500

    Create MatrixExpo.cpp

---
+++ MATHEMATICS/MatrixExpo.cpp
@@ -0,0 +1,18 @@
+template<class T> struct Matrix{
+    vector<vector<T>> m;
+    int N, M;
+    Matrix(bool identity, int rows, int columns){
+        N = rows; M = columns;
+        m.resize(N, vector<T>(M, 0));
+        if(identity && N == M)
+            for(int i = 0; i < N; i++) m[i][i] = 1;
+    }
+    Matrix operator * (const Matrix &o){
+        Matrix<T> r(false, N, o.M);
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < o.M; j++){
+                for(int k = 0; k < M; k++) r.m[i][j] += (m[i][k] * o.m[k][j]);
+            }
+        return r;
+    }
+};
