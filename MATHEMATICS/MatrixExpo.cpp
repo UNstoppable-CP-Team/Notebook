7413cdbbad89b4e4c505c62c22fb05da5352d41e
Jherson Medina <38172045+JhersonMedina@users.noreply.github.com>
2021-09-13 15:40:04 -0500

    Update MatrixExpo.cpp

--- MATHEMATICS/MatrixExpo.cpp
+++ MATHEMATICS/MatrixExpo.cpp
@@ -1,3 +1,4 @@
+//NOTE: in case of needing modulo operations, use Modular template
 template<class T> struct Matrix{
     vector<vector<T>> m;
     int N, M;
@@ -10,9 +11,14 @@ template<class T> struct Matrix{
     Matrix operator * (const Matrix &o){
         Matrix<T> r(false, N, o.M);
         for(int i = 0; i < N; i++)
-            for(int j = 0; j < o.M; j++){
-                for(int k = 0; k < M; k++) r.m[i][j] += (m[i][k] * o.m[k][j]);
-            }
+            for(int j = 0; j < o.M; j++)
+                for(int k = 0; k < M; k++) r.m[i][j] = m[i][k] * o.m[k][j] + r.m[i][j];
         return r;
     }
+	friend Matrix binpow(Matrix<T> M, long long r){
+		Matrix<T> ans(true, M.N, M.N);
+		for(; r; M = M * M, r >>= 1)
+			if(r & 1) ans = ans * M;
+		return ans;
+	}
 };
