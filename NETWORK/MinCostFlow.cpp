4cab596e8a24ae62016628ffaba8b6fb917224c3
Jherson Medina <38172045+JhersonMedina@users.noreply.github.com>
2022-10-05 14:59:55 -0500

    Update MinCostFlow.cpp

--- NETWORK/MinCostFlow.cpp
+++ NETWORK/MinCostFlow.cpp
@@ -1,37 +1,32 @@
-/*
-  oo = infinity value
-  k = Given K find the the minimum cost of a flow = k
-      for maximum flow - minimum cost use k = oo, and comment line "if (flow < k) return -1; "
-  n = number of nodes in the residual network
-  edges = graph saved in edges list
-  cost and capacity global arrays
-*/
-//Complexity O(n*n*m*m)
-template<class T> struct MCF{
+template<class T> struct MinCostFlow {
 	struct Edge {
-	  int to, rev; T f, cap, cost;
-	  Edge(int to, int rev, T cap, T cost, T f = 0) : to(to), rev(rev), cap(cap), cost(cost), f(f) {}
+	  int to, rev; 
+	  T cap, cost, flow ;
+	  Edge(int to, int rev, T cap, T cost, T flow = 0) : to(to), rev(rev), cap(cap), cost(cost), flow(flow) {}
 	};
-	int n;
-	vector<vector<Edge>> g;
+	int n, s, t;
+	vector<vector<Edge>> graph;
 	void addedge(int s, int t, T cap, T cost){
-		g[s].push_back(Edge(t, g[t].size() , cap, cost));
-		g[t].push_back(Edge(s, g[s].size() -1, 0, -cost));
+		graph[s].push_back(Edge(t, graph[t].size() , cap, cost));
+		graph[t].push_back(Edge(s, graph[s].size() -1, 0, -cost));
 	}
-	MCF (int n) :n(n) { g.resize(n); }
-	void spfa(int v0, vector<T>& d, vector<int>& p) {
-		d.assign(n, oo); d[v0] = 0;
+	MinCostFlow (int n) : n(n) { 
+		graph.resize(n); 
+	}
+	bool spfa(int start, vector<T>& d, vector<int>& p) {
+		d.assign(n, oo); 
+		d[start] = 0;
 		vector<bool> inq(n,0);
 		queue<int> q;
-		q.push(v0);
+		q.push(start);
 		p.assign(n,-1);
 		while (!q.empty()) {
 		  int u = q.front();
 		  q.pop();
 		  inq[u] = 0;
-			for(int i= 0; i< g[u].size(); ++i){
-				Edge v = g[u][i];
-				if (v.cap - v.f > 0 && d[v.to] > d[u] + v.cost ) {
+			for(int i= 0; i < graph[u].size(); ++i){
+				Edge v = graph[u][i];
+				if (v.cap - v.flow > 0 && d[v.to] > d[u] + v.cost ) {
 					d[v.to] = d[u] + v.cost;
 					p[v.to] = v.rev;
 					if (!inq[v.to]) {
@@ -41,37 +36,34 @@ template<class T> struct MCF{
 				}
 			}
 		}
+		return d[t] != oo;
 	}
-	T mincostflow(ll K, int s, int t) {
+	pair <T, T>  maxflow(int source, int sink) {
+		s = source, t = sink;
 		T flow = 0, cost = 0;
 		vector<int> p;
-		vector<ll> d;
-		while (flow < K) {
-			spfa(s, d, p);
-			if (d[t] == oo) break;
-			//find max flow on that path
-			T f = K - flow;
+		vector<T> d;
+		while (spfa(s, d, p)) {
+			T newflow  = oo;
 			int cur = t;
 			while (cur != s) {
-				int u = g[cur][p[cur]].to;
-				int rev = g[cur][p[cur]].rev;
-				T c = g[u][rev].cap - g[u][rev].f;
-				f = min(f, c);
+				int u = graph[cur][p[cur]].to;
+				int rev = graph[cur][p[cur]].rev;
+				T c = graph[u][rev].cap - graph[u][rev].flow;
+				newflow  = min(newflow, c);
 				cur = u;
 			}
-			//apply flow
-			flow += f;
-			cost += f * d[t];
+			flow += newflow;
+			cost += newflow * d[t];
 			cur = t;
 			while (cur != s) {
-				int rev = g[cur][p[cur]].rev;
-				int u = g[cur][p[cur]].to;
-				g[u][rev].f += f;
-				g[cur][p[cur]].f -= f;
+				int rev = graph[cur][p[cur]].rev;
+				int u = graph[cur][p[cur]].to;
+				graph[u][rev].flow += newflow;
+				graph[cur][p[cur]].flow -= newflow;
 				cur = u;
 			}
 		}
-		if(flow< K) return -1;
-		return cost;
+		return {flow, cost};
 	}
 };
