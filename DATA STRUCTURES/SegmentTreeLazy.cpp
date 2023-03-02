6612a7d523cbcd1edc25f823569f6cd04f232c9d
Jherson Medina <38172045+JhersonMedina@users.noreply.github.com>
2023-03-01 22:13:37 -0500

    Update SegmentTreeLazy.cpp

--- DATA STRUCTURES/SegmentTreeLazy.cpp
+++ DATA STRUCTURES/SegmentTreeLazy.cpp
@@ -1,39 +1,46 @@
-template <class T> struct Stree {
+template <class T> struct SegmentTree {
 	T neut = 0; //set neuter value
 	int n; 
-	vector <T> tree; 
+	vector <T> tree, sum; 
 	vector <bool> lazy;
-	Stree (int n) : n(n) {
+	SegmentTree (int n) : n(n) {
 		tree.assign((n + 1) << 2, neut);
+		sum.assign((n + 1) << 2, neut);
 		lazy.assign((n + 1) << 2, 0);
 	}
 	int left(int u) { return u << 1; } 
 	int right(int u) { return u << 1 | 1; }
-	T merge(T a, T b) { return 0; } // set merge operation
+	T merge(T a, T b) { return a + b; } // set merge operation
 	void propagate(int u, int tl, int tr) {
 		if (!lazy[u]) return;
+		tree[u] += sum[u] * (tr - tl + 1);
 		if (tl != tr) { //seg lazy operation
+			sum[left(u)] += sum[u];
+			sum[right(u)] += sum[u];
+			lazy[left(u)] = lazy[right(u)] = 1;
 		}
-		lazy[u] = 0;
+		lazy[u] = sum[u] = 0;
 	}
 	void update(int u, int tl, int tr, int l, int r, T val) {
-		if (l > r) return;
-		if (tl == l && tr == r) {//set update operation
+		propagate(u, tl, tr);
+		if (tr < l || r < tl) return;
+		if (l <= tl && tr <= r) {
+			sum[u] += val;
 			lazy[u] = 1;
-		} else {
 			propagate(u, tl, tr);
+		} else {
 			int tm = (tl + tr) >> 1;
-			update(left(u), tl, tm, l, min(tm, r), val);
-			update(right(u), tm + 1, tr, max(l, tm + 1), r, val);
+			update(left(u), tl, tm, l, r, val);
+			update(right(u), tm + 1, tr, l, r, val);
 			tree[u] = merge(tree[left(u)], tree[right(u)]);
 		}
 	}
 	T query(int u, int tl, int tr, int l, int r) {
-		if (l > r) return neut;
-		if (tl == l && tr == r) return tree[u];
-		int tm = (tl + tr) >> 1;
+		if (tr < l || r < tl) return neut;
 		propagate(u, tl, tr);
-		return merge(query(left(u), tl, tm, l, min(tm, r)), query(right(u), tm + 1, tr, max(l, tm + 1), r));
+		if (l <= tl && tr <= r) return tree[u];
+		int tm = (tl + tr) >> 1;
+		return merge(query(left(u), tl, tm, l, r), query(right(u), tm + 1, tr, l, r));
 	}
 	void update(int l, int r, T val) { update(1, 0, n - 1, l, r, val); }
 	T query(int l, int r) { return query(1, 0, n - 1, l, r); }
